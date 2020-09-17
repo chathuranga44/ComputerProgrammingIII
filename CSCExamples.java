ee56faad00ecd93075d86d06a2384008ba437796
= <=>
2020-09-18 04:38:22 +0530

    added structure to CSCExamples.java

---
+++ CSCExamples.java
@@ -0,0 +1,19 @@
+public class CSCExamples {
+
+	private int QuestionNo;
+	private String category;
+	private String text;
+
+	public void setQuestionNo(int questionNo) {
+
+	}
+
+	public void setCategory(String category) {
+
+	}
+
+	public void setText(String text) {
+
+	}
+
+}
